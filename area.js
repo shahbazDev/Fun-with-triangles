49f0202124e41a9ae8b162e3094fe0a89f034cbf
Shabash Khan <shabashkhan.dev@gmail.com>
2022-10-08 00:18:32 +0530

    refactor

--- area.js
+++ area.js
@@ -3,16 +3,22 @@ const b = document.querySelector("#height1");
 const calcBtn = document.querySelector("#calc-btn1");
 const outputEl = document.querySelector("#output-area");
 
-function calculateBH(a,b) {
-    const prod = (a * b);
-    return prod;
-  }
 
 function calcArea(){
-const bh=calculateBH(Number(a.value),Number(b.value))
-var area = (1/2)*(bh);
-outputEl.innerText = `Area of Triangle is ${area}`;
 
+if(((Number(a.value)) <= 0 || ((Number(b.value))) <= 0)){
+  outputEl.innerText = "Invalid input , number should be >0"
 }
+else{
+var area = (1/2)*((Number(a.value)) *(Number(b.value)));
+outputEl.innerText = `Area of Triangle is ${area}`;
+}
+}
+
+
+
+
+
+
  calcBtn.addEventListener("click",calcArea);
 
--- hypotenuse.js
+++ hypotenuse.js
@@ -3,16 +3,27 @@ const b = document.querySelector("#height");
 const calcBtn = document.querySelector("#calc-btn");
 const outputEl = document.querySelector("#outputel");
 
+
 function calculateSumOfSquare(a,b) {
-    const sumOfSquares = (a * a + b * b);
-    return sumOfSquares;
-  }
+  const sumOfSquares = (a * a + b * b);
+  return sumOfSquares;
+}
 
 function calcHypotenuse(){
-const sumOfSquares=calculateSumOfSquare(Number(a.value),Number(b.value))
-var c=Math.round(Math.sqrt(sumOfSquares));
-outputEl.innerText="the hypotenuse side is "+ c ;
+  if(((Number(a.value)) <= 0 || ((Number(b.value))) <= 0)){
+    outputEl.innerText = "Invalid input , number should be >0"
+  }
+  else{
+    const sumOfSquares=calculateSumOfSquare(Number(a.value),Number(b.value))
+    var c=Math.round(Math.sqrt(sumOfSquares));
+    outputEl.innerText="the hypotenuse side is "+ c ;
+  }
+
+
+
+
 
 }
- calcBtn.addEventListener("click",calcHypotenuse);
+calcBtn.addEventListener("click",calcHypotenuse);
 
+ 
